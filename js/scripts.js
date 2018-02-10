c8be0457e8271fc57e1359d443dce879bbc4f514
Paolo-Turchioe <pao.turchioe@gmail.com>
2018-02-10 18:09:22 -0500

    fix 2

--- js/scripts.js
+++ js/scripts.js
@@ -10,31 +10,14 @@ $(document).ready(function(){
 		$("#"+tab_id).addClass('current');
 
 
-if(tab_id=="tab-1"){
-	$('#bxSlider1').bxSlider({
-		mode: 'fade',
-    captions: true,
-    slideWidth: 300
-	})
-}
-
-if(tab_id=='tab-2'){
-	$('#bxSlider2').bxSlider({
-		mode: 'fade',
-    captions: true,
-    slideWidth: 300
-	})
-}
-
-if(tab_id=="tab-3"){
-	$('#bxSlider2').bxSlider({
-		mode: 'fade',
-    captions: true,
-    slideWidth: 300
-	})
-}
+		$(function() {
+		  $('.bxslider').bxSlider({
+		    mode: 'fade',
+		    captions: true,
+		    slideWidth: 300
+		  });
+		});
 
 	})
 
   });
-});
